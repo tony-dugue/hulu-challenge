b0efd6fd780cdf3ce9d082bcdb85aedf0777db87
tony dugué <duguetony@gmail.com>
2021-02-25 01:04:11 +0100

    update: modification des requêtes (langue films)

--- src/API/requests.js
+++ src/API/requests.js
@@ -3,13 +3,13 @@ const API_KEY = '';
 export default {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=fr-FR`,
     fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=fr-FR`,
-    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-    fetchMystery: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
-    fetchSciFi: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
-    fetchWestern: `/discover/movie?api_key=${API_KEY}&with_genres=37`,
-    fetchAnimation: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
-    fetchTV: `/discover/movie?api_key=${API_KEY}&with_genres=10770`
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28&language=fr-FR`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35&language=fr-FR`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27&language=fr-FR`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749&language=fr-FR`,
+    fetchMystery: `/discover/movie?api_key=${API_KEY}&with_genres=9648&language=fr-FR`,
+    fetchSciFi: `/discover/movie?api_key=${API_KEY}&with_genres=878&language=fr-FR`,
+    fetchWestern: `/discover/movie?api_key=${API_KEY}&with_genres=37&language=fr-FR`,
+    fetchAnimation: `/discover/movie?api_key=${API_KEY}&with_genres=16&language=fr-FR`,
+    fetchTV: `/discover/movie?api_key=${API_KEY}&with_genres=10770&language=fr-FR`
 }
